9f21887a12411637f0781b6da81c369e7b2509ee
danilhordiienko <165608127+danilhordiienko@users.noreply.github.com>
2025-04-28 01:03:45 +0000

    Implemented Random Fact feature on Home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonicModule, HttpClientModule, CommonModule],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
-  constructor() {}
+  fact: string = '';
+  deviceInfo: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  getRandomFact() {
+    this.http.get<any>('https://uselessfacts.jsph.pl/random.json?language=en')
+      .subscribe(data => {
+        this.fact = data.text;
+      });
+  }
+
+  getDeviceInfo() {
+    this.deviceInfo = `Platform: ${navigator.platform}, UserAgent: ${navigator.userAgent}`;
+  }
 }
